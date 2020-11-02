4c96e3f0e7707f01c9c2ba93164d9e599f80dddf
Erudigit <duguet.vincent@outlook.fr>
2020-11-02 18:36:15 +0100

    New Commit

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,4 @@
 class Attendance < ApplicationRecord
-    belongs_to :attendance
-    belongs_to :user
+    belongs_to :attendance, optional: true
+    belongs_to :user, optional: true
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,10 +5,8 @@ class Event < ApplicationRecord
 
     validates :start_date, presence: true
     validates :duration, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
-    validates :title, presence: true, length: { in: 5..140 }
-    validates :description, presence: true, length { in: 20..1000 }
-    validates :price, presence: true, length { in: 1..1000}
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
     validates :location, presence: true
-
-
 end
