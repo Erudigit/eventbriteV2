cb3f91018020c05195b7bfb4839ffd7ed7daae21
Erudigit <duguet.vincent@outlook.fr>
2020-11-02 19:21:45 +0100

    fixe something

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,12 +8,5 @@ class Event < ApplicationRecord
     validates :title, presence: true
     validates :description, presence: true
     validates :price, presence: true
-    validates :location, presence: true
-
-    after_create :new_event_send
-      
-    def new_event_send
-        IntendanceMailer.new_event_email(self).deliver_now
-    end
-      
+    validates :location, presence: true      
 end
