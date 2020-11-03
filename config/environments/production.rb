88745f720af4996920cbd55d6313236596154c94
Erudigit <duguet.vincent@outlook.fr>
2020-11-03 16:12:39 +0100

    add the new adress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'arcane-ridge-70160.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'infinite-taiga-20956.herokuapp.com' }
 end
